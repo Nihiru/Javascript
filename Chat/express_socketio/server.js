406f15dc2e7578cdd55f94382eece7fb5d0ea738
Nihiru <nikhil.m.alors@gmail.com>
2020-06-14 20:30:03 +0530

    socketio with express framework

---
+++ Chat/express_socketio/server.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+  app = express(),
+  http = require("http"),
+  socketIO = require("socket.io"),
+  server,
+  io;
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.emit("greeting-from-server", () => {
+    greeting: "Hello client";
+  });
+
+  socket.on("greeting-from-client", (message) => console.log(message));
+});
