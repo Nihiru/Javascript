a4705c4fe8cf1f2ee6b1f5e673fe8f9bf1dde187
Nihiru <nikhil.m.alors@gmail.com>
2020-06-26 01:07:27 +0530

    Adding user to chat application

--- Chat/Javascript Mastery/Chat app/client/src/components/Chat/Chat.js
+++ Chat/Javascript Mastery/Chat app/client/src/components/Chat/Chat.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+    // perform an action after emitting an event
+    socket.emit("join", { name, room }, () => {
+      // alert(error);
+    });
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
   return <h1>Chat</h1>;
 };
 
--- Chat/Javascript Mastery/Chat app/server/index.js
+++ Chat/Javascript Mastery/Chat app/server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -12,6 +14,20 @@ const io = socketio(server);
 
 io.on("connection", (socket) => {
   console.log("Client and server has been connected");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log("socket id : ", socket.id, "Name: ", name, "Room: ", room);
+
+    // creating a user using a helper functions
+    // socket.id is created as soon as client establishes a connection with server
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+  });
+
   socket.on("disconnect", () => {
     console.log("Client and server has been disconnected");
   });
