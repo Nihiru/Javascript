7a8be31af879279c810a8174fb3e02ad01cc5758
Nihiru <nikhil.m.alors@gmail.com>
2020-07-02 19:54:01 +0530

    Blog Part 1

---
+++ Microservices/blog/comments/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen("4001", () => console.log("Listening to port: 4001"));
---
+++ Microservices/blog/posts/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => console.log("Listening to port: 4000"));
