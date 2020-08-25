6be5624b8b2a5c654d051ae36b467dbfa752f50f
Nihiru <nikhil.m.alors@gmail.com>
2020-08-25 16:57:29 +0530

    Aysnc/Await, Quokka.js

---
+++ Currency Converter/Async-Await.js
@@ -0,0 +1,6 @@
+const axios = require('axios')
+
+const FIXER_API_KEY = 'aa0060c3047f8a7b7fb88aafc28bc499'
+const FIXER_API = 'http://data.fixer.io/api/latest?access_key=aa0060c3047f8a7b7fb88aafc28bc499&format=1'
+
+const REST_COUNTRIES_API = 'https://restcountries.eu/rest/v2/currency/'
