6a2f1a34e6498f7e4783c9b865b2b21fef72525f
Nihiru <nikhil.m.alors@gmail.com>
2020-08-27 08:41:55 +0530

    WIP: DS & Algo - class 1

--- Currency Converter/Async-Await.js
+++ Currency Converter/Async-Await.js
@@ -1,6 +1,20 @@
 const axios = require('axios')
 
 const FIXER_API_KEY = 'aa0060c3047f8a7b7fb88aafc28bc499'
-const FIXER_API = 'http://data.fixer.io/api/latest?access_key=aa0060c3047f8a7b7fb88aafc28bc499&format=1'
+const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}&format=1`
 
 const REST_COUNTRIES_API = 'https://restcountries.eu/rest/v2/currency/'
+
+// Async/Await
+
+// Fetch data about currencies
+const getExchangeRate = (fromCurrency, toCurrency) => {
+    const {data: {rates}} = await axios.get(FIXER_API)
+    const euro = 1/ rates[fromCurrency]
+    const exchangeRate = euro * rates[toCurrency]
+}
+
+getExchangeRate('USD','EUR')
+// Fectch data about countries
+
+//
---
+++ dist/Snippets_1.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// finding a sum of n numbers
+// traditional method
+function addUptoN(n) {
+  var total = 0;
+
+  for (var i = 1; i <= n; i++) {
+    total += i;
+  }
+
+  return total;
+} // clever method
+
+
+function optimizedAddUptoN(n) {
+  return n * (n + 1) / 2;
+}
+
+var t1 = performance.now();
+optimizedAddUptoN(1000000000);
+var t2 = performance.now();
+console.log("Time taken ".concat((t2 - t1) / 100000, " seconds"));
