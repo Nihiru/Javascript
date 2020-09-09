b38f11373ab05f93b9603a7ea1532ddc1246924f
Nihiru <nikhil.m.alors@gmail.com>
2020-09-09 20:05:36 +0530

    Repeated Strings with comments

--- hackerrank/RepeatedString.js
+++ hackerrank/RepeatedString.js
@@ -5,10 +5,18 @@ function RepeatedString(s, n){
         // repeat strings upto n
         // repeatedStrings = s.padEnd(n, s)  
         let occurrencesOfAs = [...s].filter(e => e === 'a').length;
+
+        /**
+         * Math.floor(n/s.length) - is used to find out the number of occurrences of that word in `n` space
+         * occurrencesOfAs - is used count the number of `a`s in the entire calculated string of `n` space
+         */
         numOfAs = Math.floor(n/s.length) * occurrencesOfAs
     }
+    // to find out the substrings that needs to be put so as to be part of `n` space
     let tailStringLength = n % s.length;
+    // tailStringLength defines the lenght of the substring from the `s` string
     for(let i=0; i< tailStringLength;i++){
+        // finding the number of  `a`s in the substring
         if(s[i]==='a') numOfAs++;
     }
    
@@ -17,5 +25,5 @@ function RepeatedString(s, n){
 }
 
 // console.log(RepeatedString('abc', 10))
-// console.log(RepeatedString('aba', 10))
-console.log(RepeatedString('a', 1000000000000)) // failed for padEnd method
+console.log(RepeatedString('aba', 10))
+// console.log(RepeatedString('a', 1000000000000)) // failed for padEnd method
