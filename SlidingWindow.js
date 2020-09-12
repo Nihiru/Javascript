389f1558f548b097a20684ffa28b6fd97eb33aea
nihiru <nikhil.m.alors@gmail.com>
2020-09-12 18:05:23 +0530

    Sliding window implemented

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -23,3 +23,25 @@ function maxSubArraySum(arr, n){
     }
     return max
 }
+
+// sliding window 
+// time complexity - O(n)
+function maxSubArraySumRefactored(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num )return null;
+    for(let i=0;i<num ;i++){
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum
+    for (let i=num;i<arr.length ;i++){
+        tempSum = tempSum - arr[i-num]+ arr[i];
+        maxSum = Math.max(maxSum, tempSum)
+    }
+
+    return maxSum
+
+}
+
+console.log(maxSubArraySumRefactored([2,6,9,2,1,8,5,6,3], 3))
