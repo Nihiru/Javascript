f0ea4c5fe609bdded652b2a02088417d4fde4e2a
nihiru <nikhil.m.alors@gmail.com>
2020-10-25 22:18:12 +0530

    VueJS experiment app  setup

---
+++ quiz/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="App">
+    <Header />
+    <QuestionBox />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    Header,
+    QuestionBox,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ quiz/src/components/QuestionBox.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1> 
+        Question Box 
+    </h1>
+</template>
