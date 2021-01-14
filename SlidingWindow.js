72191ee32ceb18e15e8e0feead779b1ec7d3e774
Nikhil M <nikhil.m.alors@gmail.com>
2021-01-14 11:38:11 +0530

    Repo Check

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -3,22 +3,23 @@
  * @param {Array} arr - The array from which the sum needs to be calculated 
  * @param {Integer} n - Maximum sum of `n` consecutive elements in the array
  */
-function maxSubArraySum(arr, n){
-    if (num > arr.length){
+function maxSubArraySum(arr, n) {
+    if (num > arr.length) {
         return null
+
     }
 
     var max = -Infinity;
     // start from first and end upto a moment where adding current index with `n` would point to the last index in the array
-    for(let i=0;i<arr.length - num + 1;i++){
+    for (let i = 0; i < arr.length - num + 1; i++) {
         temp = 0;
         // loop only through 4 items from current index position `i`
-        for(let j=0;j<num ;j++){
-            temp +=arr[i+j]
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j]
         }
         // find the maximum for each set of 4 added numbers
-        if(temp> max){
-            max= temp
+        if (temp > max) {
+            max = temp
         }
     }
     return max
@@ -26,17 +27,17 @@ function maxSubArraySum(arr, n){
 
 // sliding window 
 // time complexity - O(n)
-function maxSubArraySumRefactored(arr, num){
+function maxSubArraySumRefactored(arr, num) {
     let maxSum = 0;
     let tempSum = 0;
-    if(arr.length < num )return null;
-    for(let i=0;i<num ;i++){
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
         maxSum += arr[i];
     }
 
     tempSum = maxSum
-    for (let i=num;i<arr.length ;i++){
-        tempSum = tempSum - arr[i-num]+ arr[i];
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
         maxSum = Math.max(maxSum, tempSum)
     }
 
@@ -44,4 +45,4 @@ function maxSubArraySumRefactored(arr, num){
 
 }
 
-console.log(maxSubArraySumRefactored([2,6,9,2,1,8,5,6,3], 3))
+console.log(maxSubArraySumRefactored([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
