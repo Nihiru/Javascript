cb5352eb14d0cfa85b0dfcf6ba56e3a244462ea7
Nikhil M <nikhil.m.alors@gmail.com>
2021-02-21 12:47:24 +0530

    feat: EventEmitter with async

--- OS/EventEmitter.js
+++ OS/EventEmitter.js
@@ -1,3 +1,12 @@
+/**
+ * -) EventEmitter is a module that facilitates communication between objects in Node.
+ * -) It as the core of Node asynchronous event-driven architecture
+ * -) emitter objects emit named emits that cause previously registered listeners to be called. features of 
+ *    emitter object
+ *      -) Emitting name events
+ *      -) Registering and unregistering listener functions 
+ */
+const fs = require('fs')
 const { EventEmitter } = require('events');
 const EventsEmitterObject = require('events')
 
@@ -16,5 +25,38 @@ const withLog = new WithLog()
 withLog.on('being', () => console.log('About to execute'))
 withLog.on('end', () => console.log('Done with execution'))
 
-withLog.execute(() => console.log('*** Executing task ***'))
+// withLog.execute(() => console.log('*** Executing task ***'))
 
+// asynchronous call
+class WithTime extends EventEmitter {
+    execute(asyncFunc, ...args) {
+        this.emit('begin')
+        console.time('execute')
+        asyncFunc(...args, (err, data) => {
+            if (err) return this.emit('error', err) // node exits the process
+            this.emit('data', data)
+            console.timeEnd('execute')
+            this.emit('end')
+        })
+    }
+    // if the async function supported promises then callbacks can be ignored
+    // async execute(asyncFunc, ...args) {
+    //     this.emit('begin');
+    //     try {
+    //         console.time('execute')
+    //         const data = await asyncFunc(...args)
+    //         this.emit('data', data)
+    //         console.timeEnd('execute')
+    //         this.emit('end')
+    //     } catch (err) {
+    //         this.emit('error', err)
+    //     }
+    // }
+}
+
+let withTime = new WithTime()
+withTime.on('begin', () => console.log('About to execute'))
+withTime.on('end', () => console.log('Done with execute'))
+withTime.on('error', (err) => console.log(err)) // this event listener will be trigger if bad argument is passed
+withTime.execute(fs.readFile, __filename)
+withTime.execute(fs.readFile, '') // to pass bad argument
