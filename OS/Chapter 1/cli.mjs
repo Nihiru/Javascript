b4441fb555b8343d5c3fc614150e87737648a791
Nikhil M <nikhil.m.alors@gmail.com>
2021-03-11 12:38:41 +0530

    ES6 edition

---
+++ OS/Chapter 1/cli.mjs
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+const [, , ...args] = process.argv
+
+console.log(`Received arguments...${args}`)
+
+// to spawn sub-processes
+import { spawn } from 'child_process';
+import { stderr } from 'process';
+
+const ls = spawn('ls', ['-lh', '/usr'])
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+})
+
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${stderr}`)
+})
+
+ls.on('close', (code) => {
+    console.log(`child process exited with ${code}`)
+})
+
+console.log(`current process ID: ${process.pid}`)
+console.log(`parent process ID: ${process.ppid}`)
+console.log(`Information about the process: ${process.report}`)
